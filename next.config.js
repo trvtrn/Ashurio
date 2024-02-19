f0e21fe38d04d51757f3fea952331fc054438c9f
Travolta Tran <z5205271@ad.unsw.edu.au>
2024-02-19 17:46:30 +1100

    env variables changed

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "https://ashurio.vercel.app",
+        hostname: "https://ashurio-production.up.railway.app",
       },
     ],
   },
